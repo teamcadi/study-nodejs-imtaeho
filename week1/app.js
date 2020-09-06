28140ad83a84e4da9387636118f770724110d771
bbaktaeho <lth9036@gmail.com>
2020-09-06 23:22:11 +0900

    Update: Comment

--- week1/app.js
+++ week1/app.js
@@ -4,6 +4,7 @@ const app = express();
 /**
  * @description 최상단 경로이므로 루트 경로라고 함 (웹사이트라면 홈페이지)
  * @route GET /
+ * todo : queryString
  */
 app.get("/", (req, res) => {
   // const a = req.query.a;
@@ -15,6 +16,11 @@ app.get("/", (req, res) => {
   res.send(`홈 페이지 ${result}`);
 });
 
+/**
+ * @description parameter 맛보기
+ * @route GET /:name
+ * todo: parameter
+ */
 app.get("/:name", function (req, res) {
   // const name = req.params.name 코드와 동일
   const { name } = req.params;
