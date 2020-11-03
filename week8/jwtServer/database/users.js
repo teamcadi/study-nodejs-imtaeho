4c9636f1946d4490443dcf97117d8c3e2bef88a0
bbaktaeho <lth9036@gmail.com>
2020-11-03 21:04:46 +0900

    week8

---
+++ week8/jwtServer/database/users.js
@@ -0,0 +1,10 @@
+/**
+ * id -> primary key (auto increment)
+ * email -> unique key
+ */
+const users = [
+  { id: 1, email: 'test@test', password: 'dsfsfg' },
+  { id: 2, email: 'test2@test2', password: 'dghdhg' },
+];
+
+module.exports = users;
